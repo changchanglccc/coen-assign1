0ed4cf9e27abe47dd6195688ffe790fefc2a997c
changchanglccc <lichong06091992@gmail.com>
2017-10-22 02:21:38 -0400

    add data file + frontend need to send json/protocol file

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 #encoding: utf-8
 
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify,request
+from flask.views import MethodView
+
 
 app = Flask(__name__)
 
@@ -8,30 +10,96 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    books = [
+    products = [
         {
-            'name': u'bookA',
-            'author': u'A',
-            'price':100
+            'number': u'1',
+            'category': u'apple'
         },
         {
-            'name': u'bookB',
-            'author': u'B',
-            'price': 200
+            'number': u'1',
+            'category': u'banana'
         },
         {
-            'name': u'bookC',
-            'author': u'C',
-            'price': 300
+            'number': u'2',
+            'category': u'beef'
         },
         {
-            'name': u'bookC',
-            'author': u'C',
-            'price': 400
+            'number': u'2',
+            'category': u'chicken'
+        },
+        {
+            'number': u'3',
+            'category': u'milk'
+        },
+        {
+            'number': u'3',
+            'category': u'yogurt'
         }
     ]
 
-    return render_template('index.html',books=books)
+    return render_template('index.html', products=products)
+
+@app.route('/register', methods=['POST'])
+def register():
+    print request.headers
+    print request.form
+    print request.form['name']
+    print request.form.get('name')
+    print request.form.getlist('name')
+    print request.form.get('nickname', default='little apple')
+    return 'welcome'
+
+
+
+@app.route('/', methods=['GET','POST'])
+def index_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
+
+
+
+class GenericRequest(MethodView):
+    def get(self):
+        name = request.args.get('name', 'Damyan')
+        return 'Hello GET from %s!' % name
+
+    def post(self):
+        name = request.form.get('name', 'Damyan')
+        return 'Hello POST from %s' % name
+
+app.add_url_rule(
+    '/generic_request',
+    view_func=GenericRequest.as_view('generic_request')
+)
+
+
+
+
+
+
+
+
+
+
+# @app.route('/login', methods=['POST', 'GET'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if valid_login(request.form['username'],
+#                        request.form['password']):
+#             return log_the_user_in(request.form['username'])
+#         else:
+#             error = 'Invalid username/password'
+#     # the code below is executed if the request method
+#     # was GET or the credentials were invalid
+#     return render_template('login.html', error=error)
+
+
+@app.route("/column/")
+def column():
+    data = [{"data": [49.9, 54.4], "name": "Tokyo"}, {"data": [42, 30.4], "name": "AC"}]
+    return render_template('column.html', data=data)
 
 
     # user = {
@@ -46,6 +114,13 @@ def index():
     #     print k
     #
     # return render_template('index.html',user=user,websites=websites)
+
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 2, type=int)
+    b = request.args.get('b', 3, type=int)
+    return jsonify(result=a + b)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
